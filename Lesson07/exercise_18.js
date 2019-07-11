0cde880afe2287bc063b8f045d363701975997cb
Siyuan Gao <siyuangao@gmail.com>
2019-07-10 19:50:40 -0700

    Update exercise_18.js

--- Lesson07/exercise_18.js
+++ Lesson07/exercise_18.js
@@ -11,10 +11,3 @@ sentence.toUpperCase();
 sentence.toLowerCase();
 sentence.charAt(0); // returns W
 console.log(sentence.length);
-
-let news = 'Today is August 31, 2008, the US president was George W. Bush';
-
-let datePattern = /[a-z]+ [0-9]{1,2}, [0-9]{4}/i;
-
-console.log(news.match(datePattern));
-console.log(datePattern.test(news));
