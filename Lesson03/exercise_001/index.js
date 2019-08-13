75474e79025e8f1248072e34dddb7d4d37daae35
Vinicius Isola <viniciusisola@gmail.com>
2019-08-12 21:08:37 -0400

    Add comments and refactor exercise 1 for better understanding

--- Lesson03/exercise_001/index.js
+++ Lesson03/exercise_001/index.js
@@ -1,11 +1,14 @@
+// Constants to calculate the interval based on time unit
 const timeUnits = ['Seconds', 'Minutes', 'Hours'];
 const multipliers = [1000, 60 * 1000, 3600 * 1000];
 
-const write = process.stdout.write.bind(process.stdout);
-
+// Variables that will store the application state
 let amount = null;
 let message = null;
 let timeUnit = null;
+
+// Alias to print to console
+const write = process.stdout.write.bind(process.stdout);
 
 function askForAmount(input) {
   if (input == null) {
@@ -57,28 +60,34 @@ function askForTimeUnit(input) {
 
 function setTimerAndRestart() {
   const currentMessage = message;
-  console.log(`Setting a reminder for '${message}' in ${amount} ${timeUnits[timeUnit]} from now.`);
-  setTimeout(() => write(`\n\x07Time for '${currentMessage}'\n> `), amount * multipliers[timeUnit]);
+  const unit = timeUnits[timeUnit];
+  write(`Setting reminder: '${message}' in ${amount} ${unit} from now.\n`);
+
+  let timerMessage = `\n\x07Time to '${currentMessage}'\n> `;
+  setTimeout(() => write(timerMessage), amount * multipliers[timeUnit]);
 
   amount = message = timeUnit = null;
   askForMessage();
 }
 
 function processInput(input) {
+  // Phase 1 - Collect message
   if (message == null) {
     askForMessage(input);
     input = null;
   }
 
+  // Phase 2 - Collect time unit
   if (message != null && timeUnit == null) {
     askForTimeUnit(input);
     input = null;
   }
 
+  // Phase 3 - Collect amount of time
   if (timeUnit != null && amount == null) {
     askForAmount(input);
   }
 }
 
 process.stdin.on('data', (data) => processInput(data.toString().trim()));
-processInput();
+askForMessage();
