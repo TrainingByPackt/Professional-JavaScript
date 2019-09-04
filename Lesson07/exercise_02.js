0cd0dc35aa6139af844dad5bd115ad6d96f124ab
Siyuan Gao <siyuangao@gmail.com>
2019-09-03 17:47:02 -0700

    Create exercise_02.js

---
+++ Lesson07/exercise_02.js
@@ -0,0 +1,17 @@
+let singers = [];
+singers.push('miku');
+console.log(singers);
+
+let food = new Array(3)
+food.push('burger');
+console.log(food);
+
+singers.push('me');
+console.log(singers);
+
+singers.pop();
+console.log(singers);
+
+singers.unshift('rin')
+
+console.log(singers);
