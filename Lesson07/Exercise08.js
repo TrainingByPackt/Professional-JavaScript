6a4f6982808b557aa791de41e1e8efeb5c45a712
Aditya <adityaks95@gmail.com>
2019-09-10 15:15:44 +0530

    Update Exercise08.js

--- Lesson07/Exercise08.js
+++ Lesson07/Exercise08.js
@@ -1,36 +1,30 @@
 // 1
-> const userInfo = ['John', 'chef', 34];
+const userInfo = ['John', 'chef', 34];
+
 // 2
-> function printUser(name, job, age) {
-... console.log(name + ' is working as ' + job + ' and is ' + age + ' years old');
-... }
+function printUser(name, job, age) {
+console.log(name + ' is working as ' + job + ' and is ' + age + ' years old');
+}
 
 // 3
-> printUser(...userInfo)
-John is working as chef and is 34 years old
+printUser(...userInfo)
 
 // 4
-> const detailedInfo = ['male', ...userInfo, 'July 5']
-=> [ 'male', 'John', 'chef', 34, 'July 5' ]
+const detailedInfo = ['male', ...userInfo, 'July 5']
+console.log(detailedInfo)
 
 // 5
-> let detailedInfoCopy = [ ...detailedInfo ];
-=> undefined
-> detailedInfoCopy
-=> [ 'male', 'John', 'chef', 34, 'July 5' ]
+let detailedInfoCopy = [ ...detailedInfo ];
+console.log(detailedInfoCopy)
 
 // 6
-> const userRequest = { name: 'username', type: 'update', data: 'newname'}
+const userRequest = { name: 'username', type: 'update', data: 'newname'}
 
 //7
-> const newObj = { ...userRequest }
-=> undefined
-> newObj
-=> { name: 'username', type: 'update', data: 'newname' }
+const newObj = { ...userRequest }
+console.log(newObj)
 
 //8
-> const detailedRequestObj = { data: new Date(), new: true, ...userRequest}
-=> undefined
-> detailedRequestObj
-=> { data: 'newname', new: true, name: 'username', type: 'update' }
+const detailedRequestObj = { data: new Date(), new: true, ...userRequest}
+console.log(detailedRequestObj)
 
